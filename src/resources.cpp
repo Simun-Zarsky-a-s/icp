c13624d08d92d6ce3cb234e6a1cb7dcfdefa48b0
daniel <xzarsk04@stud.fit.vutbr.cz>
2023-05-04 13:48:49 +0200

    fix: loading from QFile

--- src/resources.cpp
+++ src/resources.cpp
@@ -31,22 +31,21 @@ Resources::Resources(QString map){ ///todo
 }
 
 void Resources::dimensions(){
-    fstream new_file;
-    new_file.open(src_file.toStdString(), ios::in); //opening of the source file with map
+    Q_INIT_RESOURCE(resources);
+    QFile file(Sources::Map_file_destination);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << file.fileName();
+        qDebug() << "Failed to open file";
+        exit(0);
+    }
 
-        if(new_file.fail()){
-            std::cerr <<  "1File containing map not found.";
-            std::cout << src_file.toStdString();
-            exit(1);
-        }
+    QTextStream in(&file);
 
     string first_line;
-    getline(new_file, first_line); // get first line with size
+    QString qline = in.readLine();
+    first_line = qline.toStdString();// get first line with size
     istringstream iss(first_line); //split the line
     vector<int> size; string word;
-
-    //load the words from first line to size array
-    int bad_parameter = 1;
     int num;
     while(iss >> word){
         try{
@@ -61,14 +60,12 @@ void Resources::dimensions(){
 
         size.push_back(num);
     }
-    new_file.close();
+
     height = size[0]+2; //set height
     width = size[1]+2;  //set width
 }
 
 int Resources::get_width() const{
-    fstream new_file;
-    new_file.open(src_file.toStdString(), ios::in); //opening of the source file with map
 
     Q_INIT_RESOURCE(resources);
     QFile file(Sources::Map_file_destination);
@@ -78,17 +75,19 @@ int Resources::get_width() const{
         exit(0);
     }
 
+    QTextStream in(&file);
+
     string first_line;
-    getline(new_file, first_line); // get first line with size
+    QString qline = in.readLine();
+    first_line = qline.toStdString();// get first line with size
     istringstream iss(first_line); //split the line
     vector<int> size; string word;
 
     //load the words from first line to size array
-    int bad_parameter = 1;
     int num;
     while(iss >> word){
         try{
-            num = stoi(word); ///todo possible error here
+            num = stoi(word);
         }
         catch(invalid_argument){
             std::cerr <<
@@ -99,30 +98,31 @@ int Resources::get_width() const{
 
         size.push_back(num);
     }
-    new_file.close();
+
 
     return size[1]+2;//return width
 }
 
 
 int Resources::get_height() const{
-    fstream new_file;
-    new_file.open(src_file.toStdString(), ios::in); //opening of the source file with map
 
-    if(new_file.fail()){
-        std::cerr <<
-                "3File containing map not found.";
-        std::cout << src_file.toStdString();
-        exit(1);
+    Q_INIT_RESOURCE(resources);
+    QFile file(Sources::Map_file_destination);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << file.fileName();
+        qDebug() << "Failed to open file";
+        exit(0);
     }
 
+    QTextStream in(&file);
+
     string first_line;
-    getline(new_file, first_line); // get first line with size
+    QString qline = in.readLine();
+    first_line = qline.toStdString();// get first line with size
     istringstream iss(first_line); //split the line
     vector<int> size; string word;
-
     //load the words from first line to size array
-    int bad_parameter = 1;
+
     int num;
     while(iss >> word){
         try{
@@ -135,33 +135,34 @@ int Resources::get_height() const{
 
         size.push_back(num);
     }
-    new_file.close();
+
 
     return size[0]+2;//return height
 }
 
 void Resources::fill_matrix() {
-    fstream new_file;
-    string line;
 
     vector<vector<char>> loaded_matrix(height, vector<char>(width, 0));
-    new_file.open(src_file.toStdString(), ios::in); //opening of the source file with map
-        if(new_file.fail()){
-            std::cerr <<   "4File containing map not found.";
-            std::cout << src_file.toStdString();
-            exit(1);
 
+        Q_INIT_RESOURCE(resources);
+        QFile file(Sources::Map_file_destination);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            qDebug() << file.fileName();
+            qDebug() << "Failed to open file";
+            exit(0);
         }
 
-    if (new_file.is_open()) {
+        QTextStream in(&file);
+
+
+        QString qline = in.readLine();
 
         int row_num = 0;
 
-        getline(new_file, line);//removing first line with dimensions
         // filling the matrix
-        while (getline(new_file, line)) {
-            // check length of line
-
+        while (!in.atEnd()) {
+            QString qline = in.readLine();
+            string line = qline.toStdString();
             if(line.length() > width-2){
                 std::cerr <<"Loaded map exceeds, given width." ;
                 exit(1);
@@ -186,9 +187,7 @@ void Resources::fill_matrix() {
 
         }
         matrix = loaded_matrix;
-    }
 
-    new_file.close();
 
 }
 
@@ -270,26 +269,30 @@ vector<vector <char>> Resources::get_matrix() {
 }
 
 void Resources::fill_log_matrix() {
-    fstream new_file;
-    string line;
-    vector<vector<char>> loaded_matrix(height, vector<char>(width, 0));//helper matrix
 
-    new_file.open(Sources::Map_file_destination.toStdString(), ios::in); //opening of the source file with map
-    if(new_file.fail()){
-        std::cerr <<   "5File containing map not found.";
-        std::cout << src_file.toStdString();
-        exit(1);
+    vector<vector<char>> loaded_matrix(height, vector<char>(width, 0));
 
+    Q_INIT_RESOURCE(resources);
+    QFile file(Sources::Map_file_destination);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << file.fileName();
+        qDebug() << "Failed to open file";
+        exit(0);
     }
 
-    if (new_file.is_open()) {
+    QTextStream in(&file);
+
+
+    QString qline = in.readLine();
 
         int row_num = 0;
 
-        getline(new_file, line);
+
         // filling the matrix
-        while (getline(new_file, line) && row_num <  height-2) { //loading only the top of the file with map,
+        while (!in.atEnd() && row_num <  height-2) { //loading only the top of the file with map,
             // in the rest of the file are logs
+            QString qline = in.readLine();
+            string line = qline.toStdString();
 
             if(line.length() > width-2){ //check dimension
                 std::cerr <<"Loaded map exceeds, given width." ;
@@ -307,9 +310,7 @@ void Resources::fill_log_matrix() {
             row_num++;
         }
         matrix = loaded_matrix;
-    }
 
-    new_file.close(); //close the file anyway
 
 }
 
